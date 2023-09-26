e6c4c53972c5553476a8a27b8aa5f0418769b9d9
YordanCode <145832388+YordanCode@users.noreply.github.com>
2023-09-26 21:06:20 +0300

    Arrays exercises solutions

---
+++ Arrays - Exercises/CommonElements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace CommonElements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] firstArray, secondArray;
+            string[] separator = { " " };
+            firstArray = Console.ReadLine().Split(separator,StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            secondArray = Console.ReadLine().Split(separator,StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                for (int j = 0; j < firstArray.Length; j++)
+                {
+                    if (secondArray[i] == firstArray[j])
+                    {
+                        Console.Write(secondArray[i] + " ");
+                    }
+                }
+
+            }
+        }
+    }
+}
+
---
+++ Arrays - Exercises/Train/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Train
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numberOfWagons = int.Parse(Console.ReadLine());
+
+            int[] wagons;
+            wagons = new int[numberOfWagons];
+
+            int totalPeople = 0;
+
+            for (int i = 0; i < numberOfWagons; i++)
+            {
+                int countPeopleInWagon = int.Parse(Console.ReadLine());
+                wagons[i] = countPeopleInWagon;
+                totalPeople += countPeopleInWagon;
+                Console.Write(wagons[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(totalPeople);
+        }
+    }
+}
