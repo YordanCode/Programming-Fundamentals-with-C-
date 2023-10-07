5936f9a30a9f7561113867b0c7b8b19b083fd092
YordanCode <145832388+YordanCode@users.noreply.github.com>
2023-10-07 19:24:49 +0300

    Arrays exercises solutions

---
+++ Arrays - Exercises/EqualSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse).ToArray();
+
+        int index = FindElementWithEqualSums(numbers);
+
+        if (index != -1)
+            Console.WriteLine(index);
+        else
+            Console.WriteLine("no");
+    }
+    static int FindElementWithEqualSums(int[] arrayOfNumbers)
+    {
+        for (int i = 0; i < arrayOfNumbers.Length; i++)
+        {
+            int leftSum = arrayOfNumbers.Take(i).Sum();
+            int rightSum = arrayOfNumbers.Skip(i + 1).Sum();
+
+            if (leftSum == rightSum)
+                return i;
+        }
+
+        return -1;
+    }
+}
+
+
+
+
+
