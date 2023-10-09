8e9294363e50493fbf8b3456665868ee552370ba
YordanCode <145832388+YordanCode@users.noreply.github.com>
2023-10-09 12:39:33 +0300

    Arrays exercises solutions

---
+++ Arrays - Exercises/MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers;
+
+            numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int sequenceLenght = 1;
+            int longestSequence = 1;
+            int longestSequenceValue = 0;
+            List<int> longestSequenceContent = new();
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i + 1] == numbers[i])
+                {
+                    sequenceLenght += 1;
+                }
+                else
+                {
+                    sequenceLenght = 1;
+                }
+
+                if (sequenceLenght > longestSequence)
+                {
+                    longestSequence = sequenceLenght;
+                    longestSequenceValue = numbers[i];
+                    if (longestSequenceContent.Count > 0)
+                        longestSequenceContent.Clear();
+                    longestSequenceContent.AddRange
+                        (Enumerable.Repeat(longestSequenceValue, longestSequence));
+                }
+            }
+
+            foreach (var number in longestSequenceContent)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
+
+
+
