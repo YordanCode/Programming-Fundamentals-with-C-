c0efa6cca072aa069326b96870bc165d2720da6c
YordanCode <145832388+YordanCode@users.noreply.github.com>
2023-10-24 21:21:57 +0300

    Arrays exercises solutions

---
+++ Arrays - Exercises/TopIntegers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+
+namespace TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] separator = { " " };
+            int[] numbers = Console.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isTop = true;
+                for (int j = 1; j < numbers.Length - i; j++)
+                {
+                    if (numbers[i] > numbers[j + i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        isTop = false;
+                        break;
+                    }
+                }
+                if (isTop)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
